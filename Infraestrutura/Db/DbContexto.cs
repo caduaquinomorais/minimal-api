a649036494ef22b2bf1eead69bdee9d3ca6414dc
caduaquinomorais <caduaquino@yahoo.com>
2025-08-08 20:01:33 -0300

    Minuto 19:22 - Configurando projeto e tabela antes da migration

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using moduloApi.Dominio.Entidades;
+
 namespace moduloApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
+    public DbSet<Administrador> Administradores { get; set; } = default!;
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("String de conexão", ServerVersion.AutoDetect("String de conexão"));
+    }
 }
